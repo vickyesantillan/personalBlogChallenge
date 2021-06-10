cf04512440cc9d654fa7e6343308e981bae1ac58
vickyesantillan <vesantillan@live.com>
2021-06-10 15:31:53 -0300

    completing challenge 5, adding about and contact pages

--- app.js
+++ app.js
@@ -16,9 +16,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-  res.render('home', { paragraph: homeStartingContent });
+  res.render('home', { homeStartingContent: homeStartingContent });
 });
 
 app.listen(3000, function () {
   console.log('Server started on port 3000');
+});
+
+app.get('/about', function (req, res) {
+  res.render('about', { aboutContent: aboutContent });
+});
+
+app.get('/contact', function (req, res) {
+  res.render('contact', { contactContent });
 });
