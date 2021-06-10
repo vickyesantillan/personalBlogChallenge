c74005369352f8ee15c26192528b50180162910a
vickyesantillan <vesantillan@live.com>
2021-06-10 16:11:55 -0300

    completing challenge 8, adding the form in compose and making to work

--- app.js
+++ app.js
@@ -31,6 +31,9 @@ app.get('/compose', function (req, res) {
   res.render('compose');
 });
 
+app.post('/compose', function (req, res) {
+  console.log(req.body.compose);
+});
 app.listen(3000, function () {
   console.log('Server started on port 3000');
 });
