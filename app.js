7c9a4b688edbc030591fedd1447afb8f7915df49
vickyesantillan <vesantillan@live.com>
2021-06-30 12:31:41 -0300

    completing challenge 12, 13, 14, 15. Posting in the home page

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const contactContent =
   'Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.';
 
 const app = express();
-const posts = [];
+let posts = [];
 
 app.set('view engine', 'ejs');
 
@@ -17,8 +17,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-  res.render('home', { homeStartingContent: homeStartingContent });
-  console.log(posts);
+  res.render('home', {
+    homeStartingContent: homeStartingContent,
+    posts: posts
+  });
 });
 
 app.get('/about', function (req, res) {
